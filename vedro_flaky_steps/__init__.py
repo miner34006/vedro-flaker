70ea081fc897e891c6356b63caaec92adf2bfa22
Bogdan Polianok <miner34006@gmail.com>
2023-09-29 16:38:01 +0300

    add version

--- vedro_flaky_steps/__init__.py
+++ vedro_flaky_steps/__init__.py
@@ -2,4 +2,5 @@ from ._expected_failure import expected_failure
 from ._flaky_steps_plugin import FlakySteps, FlakyStepsPlugin
 from ._scheduler import FlakyStepsScenarioScheduler
 
+__version__ = "1.0.1"
 __all__ = ("FlakySteps", "FlakyStepsPlugin", "expected_failure", "FlakyStepsScenarioScheduler")
